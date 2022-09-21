cc398361c44e2a15fb795bfded8d632eee90c6e0
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-09-21 09:20:53 +0900

    RecyclerView Basic

--- app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
@@ -6,11 +6,34 @@ import androidx.recyclerview.widget.RecyclerView
 import com.raywenderlich.android.creatures.R
 import com.raywenderlich.android.creatures.app.inflate
 import com.raywenderlich.android.creatures.model.Creature
+import kotlinx.android.synthetic.main.activity_creature.view.*
+import kotlinx.android.synthetic.main.list_item_creature.view.*
 
 class CreatureAdapter(private val creatures: List<Creature>): RecyclerView.Adapter<CreatureAdapter.ViewHolder>() {
 
-    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+    class ViewHolder(v: View) : View.OnClickListener, RecyclerView.ViewHolder(v) {
+        private lateinit var creature: Creature
 
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(creature: Creature) {
+            this.creature = creature
+            val context = itemView.context
+            itemView.creatureImage.setImageResource(context.resources.getIdentifier(creature.uri, null, context.packageName))
+            itemView.fullNameText.text = creature.fullName
+            itemView.nickname.text = creature.nickname
+
+        }
+
+        override fun onClick(view: View?) {
+            view?.let {
+                val context = it.context
+                val intent = CreatureActivity.newIntent(context, creature.id)
+                context.startActivity(intent)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -18,7 +41,7 @@ class CreatureAdapter(private val creatures: List<Creature>): RecyclerView.Adapt
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        holder.bind(creatures[position])
     }
 
     override fun getItemCount() = creatures.count()
